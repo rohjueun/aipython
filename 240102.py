8fc88c05dfe75307ebe2782d619192fdcbe51bdf
rohjueun <109801116+rohjueun@users.noreply.github.com>
2024-01-02 21:38:28 +0900

    Add files via upload

---
+++ 240102.py
@@ -0,0 +1,63 @@
+number = 20
+
+
+
+result = number + 2
+
+#변수명은 예약어 아니고 식별자임
+'''
+a = '3'
+b = int(a)
+
+
+a = int(input("당신의 나이는?"))
+print(a,"살 이군요")
+print("내년엔",a+1,"살이 되네요")
+
+
+a = int(input("당신의 나이는?:"))
+msg = str(a)+"살 이군요" #같은 자료형끼리 연산가능
+print(msg)
+print("내년엔",a+1,"살 이군요")
+
+ 
+a = "파이썬"
+b = 20
+print(a,"아 반가워")
+msg = a + "아 반가워"
+print(msg)
+print("%s아 반가워"%a) #string
+print("%s아 반가워~ %d살 이구나"%(a,b))
+
+
+name = "홍길동"
+age = 20.5
+
+print("이름은 {:10s}이고 나이는 {:.2f}살이야".format(name,age))
+'''
+
+#2-6
+Lab = 105
+print('우리가 수업하는 실습실:')
+print(Lab)
+
+#2-7
+#하나의 변수를 활용하는 코드
+Lab = 105
+print('우리가 수업하는 실습실:',Lab)
+
+#2-8
+#변수 두 개를 생성하고 값을 할당
+my_id = 202011117
+grade = 3
+
+#두개의 변수에 할당된 값을 출력
+print('나의 학번:')
+print(my_id)
+print("학년:")
+print(grade)
+
+#2-9
+dollars = 2.75
+print('I have',dollars,'in my account')
+
